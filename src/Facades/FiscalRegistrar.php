14a57169705f938ec7a0c317e62025fff08e9d98
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2021-03-11 13:35:12 +0200

    Facade hints

--- src/Facades/FiscalRegistrar.php
+++ src/Facades/FiscalRegistrar.php
@@ -5,7 +5,15 @@ declare(strict_types=1);
 namespace TTBooking\FiscalRegistrar\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use TTBooking\FiscalRegistrar\DTO\Register\Request;
+use TTBooking\FiscalRegistrar\DTO\Register\Response;
 
+/**
+ * @method static Response sell(string $externalId, Request\Receipt $receipt)
+ * @method static Response sellRefund(string $externalId, Request\Receipt $receipt)
+ * @method static Response buy(string $externalId, Request\Receipt $receipt)
+ * @method static Response buyRefund(string $externalId, Request\Receipt $receipt)
+ */
 class FiscalRegistrar extends Facade
 {
     protected static function getFacadeAccessor(): string

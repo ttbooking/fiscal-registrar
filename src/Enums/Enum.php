cd46d1c77d7ea1b7f33f78a85f5b47e19d49cc68
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2021-04-05 11:32:57 +0200

    Bug fix

--- src/Enums/Enum.php
+++ src/Enums/Enum.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace TTBooking\FiscalRegistrar\Enums;
 
+use Illuminate\Contracts\Database\Eloquent\Castable;
 use MyCLabs\Enum\Enum as BaseEnum;
 use TTBooking\FiscalRegistrar\Casts\Enum as EnumCast;
 
-abstract class Enum extends BaseEnum
+abstract class Enum extends BaseEnum implements Castable
 {
     public static function castUsing(array $arguments)
     {

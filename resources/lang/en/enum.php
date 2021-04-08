18768d489a0b8ca867106db211abc26b8132f4aa
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2021-04-08 16:21:26 +0200

    Enum descriptions support + ru, en descriptions

---
+++ resources/lang/en/enum.php
@@ -0,0 +1,92 @@
+<?php
+
+return [
+
+    'agent_type' => [
+        'bank_paying_agent' => 'bank paying agent',
+        'bank_paying_subagent' => 'bank paying subagent',
+        'paying_agent' => 'paying agent',
+        'paying_subagent' => 'paying subagent',
+        'attorney' => 'attorney',
+        'commission_agent' => 'commission agent',
+        'another' => 'another',
+    ],
+
+    'operation' => [
+        'sell' => 'sell',
+        'sell_refund' => 'sell refund',
+        'sell_correction' => 'sell correction',
+        'buy' => 'buy',
+        'buy_refund' => 'buy refund',
+        'buy_correction' => 'buy correction',
+    ],
+
+    'payment_method' => [
+        'full_prepayment' => 'full prepayment',
+        'prepayment' => 'prepayment',
+        'advance' => 'advance',
+        'full_payment' => 'full payment',
+        'partial_payment' => 'partial payment and credit',
+        'credit' => 'credit',
+        'credit_payment' => 'credit payment',
+    ],
+
+    'payment_object' => [
+        'commodity' => 'commodity',
+        'excise' => 'excise',
+        'job' => 'job',
+        'service' => 'service',
+        'gambling_bet' => 'gambling bet',
+        'gambling_prize' => 'gambling prize',
+        'lottery' => 'lottery ticket',
+        'lottery_prize' => 'lottery prize',
+        'intellectual_activity' => 'intellectual activity',
+        'payment' => 'payment',
+        'agent_commission' => 'agent commission',
+        'composite' => 'composite payment object',
+        'another' => 'another payment object',
+        'property_right' => 'property right',
+        'non-operating_gain' => 'non-operating gain',
+        'insurance_premium' => 'insurance premium',
+        'sales_tax' => 'sales tax',
+        'resort_fee' => 'resort fee',
+    ],
+
+    'payment_type' => [
+        'cash',
+        'electronic',
+        'prepayment (advance) set off',
+        'credit',
+        'counter submission',
+    ],
+
+    'sno' => [
+        'osn' => 'general tax system',
+        'usn_income' => 'simplified tax system (income)',
+        'usn_income_outcome' => 'simplified tax system (income with costs deducted)',
+        'envd' => 'unified tax on imputed income',
+        'esn' => 'unified agricultural tax',
+        'patent' => 'patent based simplified tax system',
+    ],
+
+    'sno_short' => [
+        'osn' => 'GTS',
+        'usn_income' => 'STS (income)',
+        'usn_income_outcome' => 'STS (income w/ costs deducted)',
+        'envd' => 'UTII',
+        'esn' => 'UAT',
+        'patent' => 'PTS',
+    ],
+
+    'vat_type' => [
+        'none' => 'w/o VAT',
+        'vat0' => 'VAT 0%',
+        'vat10' => 'VAT 10%',
+        'vat18' => 'VAT 18%',
+        'vat20' => 'VAT 20%',
+        'vat110' => 'VAT 10/110',
+        'vat118' => 'VAT 18/118',
+        'vat120' => 'VAT 20/120',
+    ],
+
+];
--- src/Enums/Enum.php
+++ src/Enums/Enum.php
@@ -5,11 +5,27 @@ declare(strict_types=1);
 namespace TTBooking\FiscalRegistrar\Enums;
 
 use Illuminate\Contracts\Database\Eloquent\Castable;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Str;
 use MyCLabs\Enum\Enum as BaseEnum;
 use TTBooking\FiscalRegistrar\Casts\Enum as EnumCast;
 
 abstract class Enum extends BaseEnum implements Castable
 {
+    public function getDescription(string $variant = ''): string
+    {
+        return Lang::get($this->getDescriptionKey($variant));
+    }
+
+    protected function getDescriptionKey(string $variant = ''): string
+    {
+        return implode('.', [
+            'fiscal-registrar::enum',
+            Str::snake(class_basename(static::class)).($variant ? '_'.$variant : $variant),
+            $this->getValue(),
+        ]);
+    }
+
     public static function castUsing(array $arguments)
     {
         return new EnumCast(static::class);

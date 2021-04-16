145798351592ce91791897d986409af7bf605bd0
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2021-04-16 16:58:19 +0200

    Receipt show command refactoring pt. 2

--- src/Console/ReceiptShowCommand.php
+++ src/Console/ReceiptShowCommand.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace TTBooking\FiscalRegistrar\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Str;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Helper\TableCell;
 use Symfony\Component\Console\Helper\TableCellStyle;
@@ -38,24 +40,37 @@ class ReceiptShowCommand extends Command
     {
         $receipt = $receipt->resolve($this->argument('id'))->getModel();
 
-        $rightAligned = (new TableStyle)->setPadType(STR_PAD_LEFT);
-        //$separatorStyle = (new TableStyle)->setHorizontalBorderChars('-');
+        $table = new Table($this->output);
+        $style = (new TableStyle)
+            ->setHorizontalBorderChars('═', '─')
+            ->setVerticalBorderChars('║', ' ')
+            ->setCrossingChars('─', '╔', '═', '╗', '╢', '╝', '═', '╚', '╟', '╠', '═', '╣');
 
         foreach ($receipt->data->items as $item) {
-            (new Table($this->output))
-                ->setHeaders([$item->name])
-                ->setRows([
-                    ['', sprintf('%d x %.2f', $item->quantity, $item->price)],
-                    ['стоимость', sprintf('%.2f', $item->sum)],
-                    ['предмет расчета', $item->payment_object->getDescription()],
-                    ['способ расчета', $item->payment_method->getDescription()],
-                    [new TableCell(str_repeat('-', 50), ['colspan' => 2])],
-                    //new TableSeparator(['style' => $separatorStyle]),
-                ])
-                ->setStyle('compact')
-                ->setColumnWidths([30, 20])
-                ->setColumnStyle(1, $rightAligned)
-                ->render();
+            $table->addRows([
+                [new TableCell("<info>$item->name</info>", ['colspan' => 2])],
+                ['', sprintf('%d x %.2f', $item->quantity, $item->price)],
+                [static::trans('items.sum'), sprintf('%.2f', $item->sum)],
+                [static::trans('items.payment_object'), $item->payment_object->getDescription()],
+                [static::trans('items.payment_method'), $item->payment_method->getDescription()],
+                new TableSeparator,
+            ]);
         }
+
+        $table
+            ->setHeaders([new TableCell(
+                '<comment>'.Str::upper(static::trans('__self')).'</comment>',
+                ['colspan' => 2, 'style' => new TableCellStyle(['align' => 'center'])]
+            )])
+            ->addRow([static::trans('total'), sprintf('%.2f', $receipt->data->total)])
+            ->setStyle($style)
+            ->setColumnWidths([30, 20])
+            ->setColumnStyle(1, (new TableStyle)->setPadType(STR_PAD_LEFT))
+            ->render();
+    }
+
+    protected static function trans(string $key): string
+    {
+        return Lang::get('fiscal-registrar::main.receipt.'.$key);
     }
 }

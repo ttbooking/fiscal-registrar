4ef79595295a50c4df89c534b3465d6f74a1a0af
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2021-11-03 16:22:57 +0200

    WebIF: favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <link rel="shortcut icon" href="{{ asset('/vendor/fiscal-registrar/img/favicon.png') }}" />
 
     <title>{{ __('fiscal-registrar::main.title') }}{{ config('app.name') ? ' - ' . config('app.name') : '' }}</title>
 

e97bdcd4ff96cd82d60118ec98fdcf785fee2fda
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2021-11-17 14:16:31 +0200

    WebIF: IOS - prevent scaling on input focus

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ config('app.locale', 'en') }}">
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link rel="shortcut icon" href="{{ asset('/vendor/fiscal-registrar/img/favicon.png') }}" />
 

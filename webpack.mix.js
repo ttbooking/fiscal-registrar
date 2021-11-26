ed219759f404e8669cf1f73c1751a2ba9b1a994d
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2021-11-26 11:55:07 +0200

    NPM packages updated

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
-const mix = require('laravel-mix');
-const webpack = require('webpack');
-const path = require('path');
+const mix = require('laravel-mix')
+const webpack = require('webpack')
+const path = require('path')
 
 /*
  |--------------------------------------------------------------------------

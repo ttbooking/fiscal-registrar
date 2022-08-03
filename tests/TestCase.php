3bd55d701890a7a46df196d9a26d50282339af2e
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2022-08-03 15:38:44 +0200

    Migration from Psalm to Larastan

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,13 +5,25 @@ declare(strict_types=1);
 namespace TTBooking\FiscalRegistrar\Tests;
 
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use TTBooking\FiscalRegistrar\Facades\FiscalRegistrar;
+use TTBooking\FiscalRegistrar\Facades\Receipt;
 use TTBooking\FiscalRegistrar\FiscalRegistrarServiceProvider;
+use TTBooking\FiscalRegistrar\Support\ReceiptQRCode;
 
 abstract class TestCase extends OrchestraTestCase
 {
     protected function getPackageProviders($app): array
     {
         return [FiscalRegistrarServiceProvider::class];
+    }
+
+    protected function getPackageAliases($app): array
+    {
+        return [
+            'FiscalRegistrar' => FiscalRegistrar::class,
+            'Receipt' => Receipt::class,
+            'ReceiptQRCode' => ReceiptQRCode::class,
+        ];
     }
 
     protected function getEnvironmentSetUp($app): void
